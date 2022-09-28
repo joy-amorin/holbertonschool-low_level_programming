cf219279eb79792a2b4d6fcd98b1f80853286379
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-09-28 11:22:59 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * *main-descripcion
+ * *
+ * *Return:0
+ * */
+
+int main (void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return(0	return(0))
+}
