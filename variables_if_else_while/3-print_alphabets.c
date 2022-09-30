dc6a7c6632c3cfb9e3ad0323c52524e6373b49ac
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-09-30 13:19:57 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print alphabets
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char chars;
+
+	for (chars = 97 ; chars <= 122 ; chars++)
+		putchar(chars);
+	for (chars = 65 ; chars <= 90 ; chars++)
+	putchar('\n');
+
+return (0);
+}
