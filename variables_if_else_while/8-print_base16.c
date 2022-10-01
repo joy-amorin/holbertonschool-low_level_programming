2972c7030ae67a02119b9c1dc77017be56932ead
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-01 15:09:00 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints numbers
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+	char chars;
+
+	for (; n <= 9 ; n++)
+		putchar(n);
+	for (chars = 97 ; chars <= 102 ; chars++)
+		putchar(chars);
+	putchar('\n');
+	return (0);
+}
