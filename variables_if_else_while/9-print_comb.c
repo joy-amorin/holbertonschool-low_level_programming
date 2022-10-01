bd73599a346915273215acf16c8765a47c9b96ec
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-01 15:37:45 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - print comb
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9 ; n++)
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+	}
+}
