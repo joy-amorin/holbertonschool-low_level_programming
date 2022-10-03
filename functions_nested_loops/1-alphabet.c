2a04935e01e7a2f79bd8cbe951ad35677d97aa78
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-03 15:48:01 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabets
+ */
+void print_alphabet(void)
+{	char alpha;
+		for (alpha = 97 ; alpha <= 122 ; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+}
