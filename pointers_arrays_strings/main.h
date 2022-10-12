1ca4e4fdd5c8d70c3dba97aa2d56d5ed0421727a
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-12 12:33:12 -0700

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -34,7 +34,7 @@ char *_strncat(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
-void reverse_array(int *a, int n)
+void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
