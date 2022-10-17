1107638e324f75085cfc4f14d26592cdb0ee79e8
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-17 10:13:45 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <string.h>
 /**
- * _strlen_recursion - returns the length of a string
- * @s: ponter to the string
+ * _strlen_recursion - count number of characters on a string..
+ * @s: String to count.
  *
- * Return: 0
+ * Return: Number of characters.
+ *
+ *
  */
 int _strlen_recursion(char *s)
 {
-	if (*s)
-	{
-		_strlen_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s != '\0')
+
+		return (1 + _strlen_recursion(s + 1));
+
 	return (0);
 }
