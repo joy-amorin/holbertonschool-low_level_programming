5fd6e795d681a4ac9182d922f9cada333c3a3fe5
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-26 15:08:45 -0700

    fix syntax

--- more_malloc_free/0-mallock_checked.c
+++ more_malloc_free/0-mallock_checked.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  * malloc_checked - alocates memory
- *
+ *@b: size of memory, unsigned integer
  * Return: 98 if malloc fails, pointer if not fails
  */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
-	void *ptr
-		ptr = malloc(b):
+	void *ptr = NULL;
+		ptr = malloc(b);
 			if (!ptr)
-			return (98);
+			exit (98);
 	return (ptr);
 }
