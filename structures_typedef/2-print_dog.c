f20e062cf4d7bdb1dca2464d44391240ea444196
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-27 20:32:22 -0700

    conditional operator

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,27 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
+
 /**
-* print_dog - prints a struct dog
-*
-* @d: pointer of struct dog
-*/
+  * print_dog - prints a struct dog.
+  * @d: pointer to struct dog.
+  *
+  */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
-
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");	//'?:' conditional operator
 		printf("Age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner %s\n", (d->owner) ? d->owner : "(nil)");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
-/**
- * conditional operator '?:'
- * If the condition ( Expression1 ) is True, then Expression2 will be
- * executed and the result will be returned. Otherwise,
- * if the condition ( Expression1 ) is false,
- * then Expression3 will be executed and the
- * result will be returned.
- */
 }
