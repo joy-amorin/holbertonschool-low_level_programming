7709a1ce0f219819f8e48cda5d150a0e2049dd04
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-28 11:03:55 -0700

    dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,16 +9,14 @@
   *
   * Description: name, owner and age of a dog.
   */
-
-struct dog
+typedef struct dog
 {
 	char *name, *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strdup(char *str);
 void free_dog(dog_t *d);
