aa12cccc0a773430dd3856150361705d8550d427
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-28 08:41:30 -0700

    conditional operator

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");	//'?:' conditional operator
-		printf("Age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
-	}
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %f\n", d->age != 0 ? d->age : 0);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}	/**
+		  * '?:' conditional operator
+		  *
+		  */
 }
