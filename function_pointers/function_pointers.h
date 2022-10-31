251411054be77941b27b1e899137f8c232c06a7d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-10-31 10:47:44 -0700

    stddef

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef function_pointers_h
 #define function_pointers_h
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
