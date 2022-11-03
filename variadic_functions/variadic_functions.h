65e6636fad4f22532df9154dd3aaba7680a04a9d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-03 12:00:35 -0700

    fix h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
-#define <stdarg.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
