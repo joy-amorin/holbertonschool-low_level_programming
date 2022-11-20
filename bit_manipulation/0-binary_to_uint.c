7502db6de6f8426e0fae3104bcfa67caf0bd808c
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-20 11:21:32 -0800

    fix conditional 0-1

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,8 +14,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int b2 = 1;
 
 	if (b == NULL)
+	{
 		return (0);
-
+	}
+	if ((b[i] != '0') && (b[i] != '1'))
+	{
+		return (0);
+	}
 	while (b[i] != '\0')
 	{
 		if (b[i] == '1')
@@ -24,6 +29,7 @@ unsigned int binary_to_uint(const char *b)
 		}
 	}
 	b2 = b2 * 2;
+	i--;
 
 	return (r);
 }
