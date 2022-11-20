c4fcfcd07ef2ae50eb187736b3b6cbde2b57ad30
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-20 10:07:14 -0800

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*binary_to_uint - converts a binary number to unsigned int
+* @b: pointing to a string of 0 and 1 chars
+* Return: the converter number or 0
+*
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	unsigned int i = 0;
+	unsigned int b2 = 1;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] == '1')
+		{
+			r = r + b2;
+		}
+	}
+	b2 = b2 * 2;
+
+	return (r);
+}
