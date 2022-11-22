b3bae8872617adf6893a90db8b76ec1aab606252
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-22 12:18:31 -0800

    fix write

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,7 +14,6 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int w = 0;
 	char *buf;
 
 	if (filename == NULL)
@@ -24,24 +23,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(letters + 1));
+	buf = malloc(letters);
 
 	if (buf == NULL)
 		return (0);
 	read(fd, buf, letters);
-	buf[letters + 1] = '\0';
-	close(fd);
 
-	fd = open(filename, O_WRONLY);
-	if (fd == -1)
-		return (0);
+	write(1, buf, letters);
+	/*
 	w = write(fd, stdout, letters);
-	if (w == -1)
-		return (0);
+*/
+
 	close(fd);
 	free(buf);
 
 
 	return (letters);
-
 }
