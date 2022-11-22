dc72a9ff1bde45c60d219f8c2685ea5baf888f1d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-22 09:21:51 -0800

    fix betty again

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* read_text -  reads a text file and prints it to the POSIX standard output
+* read_textfile -  reads a text file and prints it to the POSIX standard output
 * @filename: file name
 * @letters:number of letters it should read and prin
 *
