7fac52a0c2230d19f46c7f9ab82ed20b0c69c591
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-23 08:18:31 -0800

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDONLY, 0600);
+	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
