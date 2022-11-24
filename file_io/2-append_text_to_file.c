518dd3ae3dd91bdb6552d875fcaf7f82ecc4fc3d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-24 10:23:38 -0800

    fix permissions

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,12 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename)
 		return (1);
-
 	if (filename == NULL)
 		return (-1);
 
 
-	fd = open(filename, O_WRONLY, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
