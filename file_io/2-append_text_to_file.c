5ccd095239b072522606d51a4fc9079198420c43
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-24 10:09:03 -0800

    fix permissions

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
-* append_text_to_ file - append text at the end of file
+* append_text_to_file - append text at the end of file
 * @filename: name of the file
 * @text_content: the NULL terminated string to add at the end of the file
 *
@@ -14,12 +14,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int w;
 
+
 	if (filename == NULL)
 		return (-1);
-	else
-		return (1);
 
-	fd = open(filename, O_APPEND);
+
+	fd = open(filename, O_WRONLY, O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
