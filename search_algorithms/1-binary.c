9f446184c792106bac3af6c6cfdecb1f8090092f
Joy-Amorin <amorinkaren2021@gmail.com>
2023-05-25 14:50:39 -0700

    tsk1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1, mid, i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
+}
